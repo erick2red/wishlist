e49d299321d7b70daf5cdc89d0d4ef7e8361cbbd
Erick Pérez Castellanos <erick.red@gmail.com>
2013-02-08 19:01:49 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wishlist::Application.config.secret_token = '2d0f5ee7ae4b2e3325b3c0628a38cd5f8fa77bb995854e4dbcc57288ba9e5d1bfe39db2c2adde0e41fe998fa0df4f3f3839a924dc8f8d4148a7145a1eb0a9173'
